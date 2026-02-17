32ef72f48f3379d358006698c093e3d5232a3116
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-02-16 18:56:24 -0600

    Played around with pagination configuration

--- rest_tutorial/settings.py
+++ rest_tutorial/settings.py
@@ -72,6 +72,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
 }
 
 WSGI_APPLICATION = 'rest_tutorial.wsgi.application'
